c4046a82db7ddb7ed72bc5e324812c41d1dfdef2
oswystan <oswystan@126.com>
2020-11-15 09:20:25 +0800

    add file report

---
+++ README.md
@@ -0,0 +1,13 @@
+# usage
+
+```shell
+## build
+$ make
+
+## configurate the sniffer.yaml file for your services
+
+## monitor all services
+$ ./sniffer
+
+```
+
--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"sync"
 	"time"
 )
+
+type YachText struct {
+	Content string `json:"content"`
+}
+type YachData struct {
+	MsgType string   `json:"msgtype"`
+	Text    YachText `json:"text"`
+}
 
 func sniff(ch <-chan Sniffer) {
 DONE:
@@ -20,12 +29,28 @@ DONE:
 	}
 }
 
-func dumpResult(sniffers []Sniffer) {
+func consoleReport(sniffers []Sniffer) {
 	for _, one := range sniffers {
 		if one.Succ() {
 			// fmt.Printf("[SUCC] %s\n", one.Message())
 		} else {
 			fmt.Printf("[FAIL] %s\n", one.Message())
+		}
+	}
+}
+
+func fileReport(sniffers []Sniffer, fn string) {
+	file, err := os.OpenFile(fn, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	for _, one := range sniffers {
+		if one.Succ() {
+			// fmt.Printf("[SUCC] %s\n", one.Message())
+		} else {
+			file.WriteString(fmt.Sprintln(time.Now().Format("2006-01-02T15:04:05.000Z07:00"), "[FAIL]", one.Message()))
 		}
 	}
 }
@@ -50,8 +75,10 @@ func main() {
 		close(ch)
 		wg.Wait()
 
-		dumpResult(sniffers)
+		fileReport(sniffers, "./sniffer.log")
 
+		wg = nil
+		sniffers = nil
 		time.Sleep(time.Duration(conf.Interval) * time.Second)
 	}
 }
